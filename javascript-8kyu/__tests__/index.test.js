13a78f4c4466338caadfc22a07b3e94ad678525c
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2023-03-06 21:59:43 +0000

    Fix test for 01 - String repeat

--- javascript-8kyu/__tests__/index.test.js
+++ javascript-8kyu/__tests__/index.test.js
@@ -1,9 +1,9 @@
 const { repeatStr } = require("./../index");
 
 describe("01 - String repeat", () => {
-  test("basic tests", () => {
+  test.only("basic tests", () => {
     expect(repeatStr(6, "I")).toBe("IIIIII");
     expect(repeatStr(5, "Hello")).toBe("HelloHelloHelloHelloHello");
-    expect(repeatStr(2, "ha ")).toBe("Ha Ha ");
+    expect(repeatStr(2, "ha ")).toBe("ha ha ");
   });
 });
