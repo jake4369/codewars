d708513d9d543a53c024796101f635cabd3d82e7
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2023-03-06 21:50:53 +0000

    Write jest tests for String repeat kata

---
+++ javascript-8kyu/__tests__/index.test.js
@@ -0,0 +1,9 @@
+const { repeatStr } = require("./../index");
+
+describe("01 - String repeat", () => {
+  test("basic tests", () => {
+    expect(repeatStr(6, "I")).toBe("IIIIII");
+    expect(repeatStr(5, "Hello")).toBe("HelloHelloHelloHelloHello");
+    expect(repeatStr(2, "ha ")).toBe("Ha Ha ");
+  });
+});
